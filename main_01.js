116b9d1b7dddab8d536a025be0ba954115287720
IJTINK7 <ijtink7@mail.ru>
2023-05-14 20:17:20 +0300

    Enumerating data type

---
+++ main_01.js
@@ -0,0 +1,11 @@
+//I Типы данных:
+// 1) Примитивы:
+    const string = '123';
+    const number = 123;
+    const bigInt = 10n;
+    const nullType = null;
+    const undefinedType = undefined;
+    const boolean = true;
+    const symbol = symbol;
+// 2) Объекты
+    const obj = {}
