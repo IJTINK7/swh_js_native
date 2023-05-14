ad9b4979345e974fab7dfb2da783503097c49371
IJTINK7 <ijtink7@mail.ru>
2023-05-14 20:21:30 +0300

    Adding typeof info

--- main_01.js
+++ main_01.js
@@ -1,11 +1,16 @@
-//I Типы данных:
-// 1) Примитивы:
+//1) Типы данных:
+// I) Примитивы:
     const string = '123';
     const number = 123;
     const bigInt = 10n;
     const nullType = null;
     const undefinedType = undefined;
     const boolean = true;
-    const symbol = symbol;
-// 2) Объекты
+    // const symbol = symbol;
+// II) Объекты
     const obj = {}
+
+// 2)typeof - возвращает тип данных в строком виде
+console.log(typeof nullType) // "object"
+console.log(typeof (typeof nullType)) // string
+
